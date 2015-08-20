e9ad5e9f649f2b77b21651407dc411a4e6ea5534
dwoodfork <dmwoodfork@fullsail.edu>
2015-08-20 18:10:03 -0500

    Variables and Logic
    Updated the variables and created the logic for calculating

--- Conditionals/conditionals.js
+++ Conditionals/conditionals.js
@@ -5,3 +5,32 @@
  Conditionals Assignment
  */
 
+//create variables
+
+var name= prompt("Enter your first name!");
+var num1 = prompt("Enter the first number");
+var num2 = prompt("Enter the second number");
+var operation;
+
+
+
+//the logic for the calculator
+
+if (operation === "+" + (num1 >= 1) + (num2 >= 1));
+
+{
+    num1 = Number(num1);
+    num2 = Number(num2);
+    console.log(name + " " + num1 + " " + "+" + " " + num2 + " " + "equals" + " " +(num1 + num2));
+}
+if (operation === "-" + (num1 >= 1) + (num2 >= 1));
+{
+    num1 = Number(num1);
+    num2 = Number(num2);
+    console.log (name + " " + num1 + " " + "+" + " " + num2 + " " + "equals" + " " +(num1 - num2));
+}
+elseif (operation === " ");
+{
+ console.log ("You did not enter anything! Refresh and try again!");
+}
+
