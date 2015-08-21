2793f829925ef30203634f2fbdf21f20c075ef61
dwoodfork <dmwoodfork@fullsail.edu>
2015-08-20 20:08:13 -0500

    Logic Correction
    Updated the logic of the calculator

--- Conditionals/conditionals.js
+++ Conditionals/conditionals.js
@@ -8,29 +8,34 @@
 //create variables
 
 var name= prompt("Enter your first name!");
-var num1 = prompt("Enter the first number");
-var num2 = prompt("Enter the second number");
-var operation;
-
+var num1 = prompt("Enter the first number >= 1");
+var num2 = prompt("Enter the second number >= 1");
+var operation = prompt("Enter the plus (+) or minus (-) sign for your operator");
+var correct = confirm ("Are these your numbers, " + num1 + " " + num2 + "?");
 
 
 //the logic for the calculator
 
-if (operation === "+" + (num1 >= 1) + (num2 >= 1));
 
+ if ((operation === "+") && (correct == true) && (num1 >= 1 ? " Your number is acceptable!" : "Your number is not acceptable"))
 {
     num1 = Number(num1);
     num2 = Number(num2);
-    console.log(name + " " + num1 + " " + "+" + " " + num2 + " " + "equals" + " " +(num1 + num2));
-}
-if (operation === "-" + (num1 >= 1) + (num2 >= 1));
-{
-    num1 = Number(num1);
-    num2 = Number(num2);
-    console.log (name + " " + num1 + " " + "+" + " " + num2 + " " + "equals" + " " +(num1 - num2));
-}
-elseif (operation === " ");
-{
- console.log ("You did not enter anything! Refresh and try again!");
-}
 
+    console.log(name + ", " + num1 + " " + "+" + " " + num2 + " " + "equals" + " " + (num1 + num2));
+
+}
+    else if ((operation === "-") && (correct == true) && (num1 >= 1 ? " Your number is acceptable!" : "Your number is not acceptable!"))
+    {
+        num1 = Number(num1);
+        num2 = Number(num2);
+        console.log(name + ", " + num1 + " " + "-" + " " + num2 + " " + "equals" + " " + (num1 - num2));
+
+    }
+
+    else
+    {
+        (operation != "+" || "-") || (correct == false) || ((num1 < 1 ? " Your number is not acceptable" : "Your number is not acceptable"));
+        console.log(name + ", " + "refresh and try again with the correct operator and/or numbers!");
+
+    }
