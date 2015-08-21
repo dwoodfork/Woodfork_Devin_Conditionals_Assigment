a8a85aa3e9b55940b3e96282e2c441c38c5137e8
dwoodfork <dmwoodfork@fullsail.edu>
2015-08-20 20:42:12 -0500

    Update Logic
    Added a Ternary Operator correctly, corrected math and corrected output

--- Conditionals/conditionals.js
+++ Conditionals/conditionals.js
@@ -12,33 +12,33 @@ var age = prompt("Enter your age!");
 var num1 = prompt("Enter the first number >= 1");
 var num2 = prompt("Enter the second number >= 1");
 var operation = prompt("Enter the plus (+) or minus (-) sign for your operator");
-var correct = confirm ("Are these your numbers, " + num1 + " " + num2 + "?");
+var correct = confirm ("Are these your numbers, " + num1 + " & " + num2 + "?");
 
 
 
 //the logic for the calculator
 
 
- if ((operation === "+") && (correct == true))
+ if ((operation === "+") && (correct == true) && (num1 >=1) && (num2 >= 1) )
 {
     num1 = Number(num1);
     num2 = Number(num2);
 
-    console.log(name + " your age " + (age >= 18 ? "you are of age" : "you are not of age"));
+    console.log(name + " according to your age " + (age >= 18 ? "you are a legal adult" : "you are not of adult age"));
     console.log(name + ", " + num1 + " " + "+" + " " + num2 + " " + "equals" + " " + (num1 + num2));
 
 }
-    else if ((operation === "-") && (correct == true))
+    else if ((operation === "-") && (correct == true) && (num1 >=1) && (num2 >= 1))
     {
         num1 = Number(num1);
         num2 = Number(num2);
 
-        console.log(name + "  " + (age >= 18 ? "you are of age" : "you are not of age"));
+        console.log(name + " according to your age " + (age >= 18 ? "you are a legal adult" : "you are not of adult age"));
         console.log(name + ", " + num1 + " " + "-" + " " + num2 + " " + "equals" + " " + (num1 - num2));
 
     }
 
-    else if ((operation != "+" || "-") || (correct == false)){
+    else if ((operation != "+" || "-") || (correct == false) || (num1 < 1) || (num2 < 1)){
 
         console.log(name + ", " + "refresh and try again with the correct operator and/or numbers!");
 
